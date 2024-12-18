c5694059218006b52e110cb52097f670aa734941
1lostevil1 <ki-04@yandex.ru>
2024-12-18 15:56:08 +0300

    some changes in scriot

--- script.sh
+++ script.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-# Создаём новую директорию
-DEST_DIR="./backup_directory"
-mkdir -p "$DEST_DIR"
+# Определяем путь к новой директории для резервных копий
+BACKUP_DIR="./backup_directory"
 
-# Проверяем, существует ли файл ~/.bashrc
-if [ -f ~/.bashrc ]; then
-  # Копируем файл
-  if cp ~/.bashrc "$DEST_DIR/.bashrc.backup"; then
-    echo "Файл ~/.bashrc успешно скопирован в $DEST_DIR/.bashrc.backup"
+# Создаем новую директорию, если она не существует
+mkdir -p "$BACKUP_DIR"
+
+# Определяем путь к исходному файлу
+SOURCE_FILE="$HOME/.bashrc"
+
+# Определяем путь к конечному файлу с расширением .backup
+BACKUP_FILE="$BACKUP_DIR/.bashrc.backup"
+
+# Проверяем существование исходного файла
+if [ -f "$SOURCE_FILE" ]; then
+  # Копируем файл в новую директорию с новым расширением
+  cp "$SOURCE_FILE" "$BACKUP_FILE"
+
+  # Проверяем успешность операции копирования
+  if [ $? -eq 0 ]; then
+    echo "Файл $SOURCE_FILE успешно скопирован в $BACKUP_FILE"
   else
-    echo "Ошибка: не удалось скопировать файл ~/.bashrc."
+    echo "Ошибка: не удалось скопировать файл $SOURCE_FILE."
   fi
 else
-  echo "Ошибка: файл ~/.bashrc не найден."
+  echo "Ошибка: файл $SOURCE_FILE не найден."
 fi
